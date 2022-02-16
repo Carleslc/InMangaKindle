9b39bbfaae192306a18985370e27468829370bc5
Carleslc <lazaro.costa.carles@gmail.com>
2022-02-16 17:15:39 +0100

    Improved chapter intervals + refactors

--- manga.py
+++ manga.py
@@ -3,10 +3,12 @@
 
 import os
 import re
+import math
 import json
 import signal
 import argparse
 import tempfile
+import bisect
 from multiprocessing import freeze_support
 
 def install_dependencies(dependencies_file):
@@ -48,11 +50,13 @@ EXTENSION_KEEP = set('.')
 
 SCRAPER = cloudscraper.create_scraper()
 
+CHAPTERS_FORMAT = 'Format: start..end or chapters with commas. Example: --chapter 3 will download chapter 3, --chapter last will download the last chapter available, --chapters 3..last will download chapters from 3 to the last chapter, --chapter 3 will download only chapter 3, --chapters "3, 12" will download chapters 3 and 12, --chapters "3..12, 15" will download chapters from 3 to 12 and also chapter 15.'
+
 def set_args():
   global args
   parser = argparse.ArgumentParser()
-  parser.add_argument("manga", help="manga to download")
-  parser.add_argument("--chapters", "--chapter", help='chapters to download. Format: start..end or chapters with commas. Example: --chapters "3..last" will download chapters from 3 to the last chapter, --chapter 3 will download only chapter 3, --chapters "3, 12" will download chapters 3 and 12, --chapters "3..12, 15" will download chapters from 3 to 12 and also chapter 15. If this argument is not provided all chapters will be downloaded.')
+  parser.add_argument("manga", help="manga to download", nargs='+')
+  parser.add_argument("--chapters", "--chapter", help=f'chapters to download. {CHAPTERS_FORMAT} If this argument is not provided all chapters will be downloaded.', nargs='+')
   parser.add_argument("--directory", help=f"directory to save downloads. Default: {MANGA_DIR}", default=MANGA_DIR)
   parser.add_argument("--single", action='store_true', help="merge all chapters in only one file. If this argument is not provided every chapter will be in a different file")
   parser.add_argument("--rotate", action='store_true', help="rotate double pages. If this argument is not provided double pages will be splitted in 2 different pages")
@@ -63,16 +67,18 @@ def set_args():
   parser.add_argument("--remove-alpha", action='store_true', help="When converting to PDF remove alpha channel on images using ImageMagick Wand")
   args = parser.parse_args()
 
-def print_colored(message, *colors):
+def print_colored(message, *colors, end='\n'):
   def printnoln(s):
     print(s, end='', flush=True)
   for color in colors:
     printnoln(color)
-  print(message)
+  print(message, end=end)
   printnoln(Style.RESET_ALL)
 
-def error(message):
+def error(message, tip=''):
   print_colored(message, Fore.RED, Style.BRIGHT)
+  if tip:
+    print_dim(tip)
   exit()
 
 def not_found():
@@ -147,23 +153,18 @@ def manga_directory(manga):
   return f'{MANGA_DIR}/{manga}'
 
 def chapter_directory(manga, chapter):
-  return f'{manga_directory(manga)}/{chapter}'
+  return f'{manga_directory(manga)}/{chapter:g}'
 
 def check_exists_file(path):
   if os.path.isfile(path):
     print_colored(f'{path} - Already exists', Fore.YELLOW)
     return True
   return False
-
-def check_exists_dir(path):
-  if not os.path.isdir(path):
-    tip = ' Try again this command without --cache' if args.cache else ''
-    error(f'{path} is not downloaded, please download that chapter first.{tip}');
-
 def files(dir, extension=''):
+  if not os.path.isdir(dir):
+    error(f'{dir} does not exist!')
   def filename(file):
     return file.split('.')[-2]
-  check_exists_dir(dir)
   for file in os.listdir(dir):
     path = os.path.abspath(f'{dir}/{file}')
     if os.path.isfile(path) and file.endswith(extension):
@@ -196,25 +197,58 @@ def load_json(data, *keys):
     data = json.loads(data.get(key))
   return data.get(keys[-1])
 
-def parse_chapters_range(chapters, last):
-  global CHAPTERS
+def parse_chapter_intervals(chapter_intervals_str, last, start_end_sep='..', interval_sep=','):
+  def parse_chapter(chapter):
+    return last if chapter == 'last' else float(chapter)
+  
+  def parse_chapter_interval(chapter_interval_str):
+    boundaries = chapter_interval_str.strip().split(start_end_sep)
 
-  def get_number(number):
-    return last if number == 'last' else int(number)
+    start_chapter = parse_chapter(boundaries[0])
+    end_chapter = start_chapter
+    
+    for chapter in boundaries[1:]:
+      chapter = parse_chapter(chapter)
+      if chapter < start_chapter:
+        start_chapter = chapter
+      elif chapter > end_chapter:
+        end_chapter = chapter
+    
+    return start_chapter, end_chapter
 
-  ranges = chapters.replace(' ', '').split(',')
   try:
-    for r in ranges:
-      split = r.split('..')
-      if len(split) == 1:
-        CHAPTERS.add(get_number(split[0]))
-      else:
-        CHAPTERS.update(range(get_number(split[0]), get_number(split[1]) + 1))
+    return merge_intervals(map(parse_chapter_interval, chapter_intervals_str.split(interval_sep)))
   except ValueError:
-    error('Invalid chapters format')
+    error(f'Invalid chapters format', CHAPTERS_FORMAT)
 
-def filename_chapter_range(sorted_chapters):
-  ranges = [] # list[(start, end)]
+def merge_intervals(chapter_intervals):
+  # convert to list and sort intervals by start so overlapping intervals are next to each other
+  overlapping_intervals = sorted(chapter_intervals, key=lambda chapter_interval: chapter_interval[0])
+
+  # merge overlapping intervals to remove redundancy
+
+  if len(overlapping_intervals) <= 1:
+    return overlapping_intervals
+
+  chapter_intervals = []
+  current_start, current_end = overlapping_intervals[0]
+
+  for other_start, other_end in overlapping_intervals[1:]:
+    if other_start <= current_end and other_end >= current_start: # overlaps
+      current_start = min(current_start, other_start)
+      current_end = max(current_end, other_end)
+    else:
+      chapter_intervals.append((current_start, current_end))
+      current_start = other_start
+      current_end = other_end
+  
+  if not chapter_intervals or chapter_intervals[-1][1] != current_end: # last
+    chapter_intervals.append((current_start, current_end))
+  
+  return chapter_intervals
+
+def get_chapter_intervals(sorted_chapters):
+  chapter_intervals = [] # list[(start, end)]
 
   if len(sorted_chapters) > 0:
     start_chapter = sorted_chapters[0]
@@ -222,13 +256,77 @@ def filename_chapter_range(sorted_chapters):
 
     for chapter in sorted_chapters:
       if chapter > end_chapter + 1:
-        ranges.append((start_chapter, end_chapter))
+        chapter_intervals.append((start_chapter, end_chapter))
         start_chapter = chapter
       end_chapter = chapter
     
-    ranges.append((start_chapter, end_chapter))
+    chapter_intervals.append((start_chapter, end_chapter))
   
-  return ','.join(map(lambda range: f'{range[0]}-{range[1]}' if range[0] != range[1] else str(range[0]), ranges))
+  return chapter_intervals
+
+def join_chapter_intervals(chapter_intervals, start_end_sep='..', interval_sep=','):
+  def chapter_interval_str(chapter_interval):
+    start, end = chapter_interval
+    return f'{start:g}{start_end_sep}{end:g}' if start != end else f'{start:g}'
+  return interval_sep.join(map(chapter_interval_str, chapter_intervals))
+
+def chapters_to_intervals_string(sorted_chapters, start_end_sep='-', interval_sep=','):
+  chapter_intervals = get_chapter_intervals(sorted_chapters)
+  return join_chapter_intervals(chapter_intervals, start_end_sep=start_end_sep, interval_sep=interval_sep)
+
+def chapters_in_intervals(sorted_all_chapters, chapter_intervals):
+  found_chapters = []
+  not_found_chapter_intervals = []
+
+  for start_chapter, end_chapter in chapter_intervals:
+    # find index of first chapter available greater or equal than start_chapter
+    i = bisect.bisect_left(sorted_all_chapters, start_chapter)
+    
+    if i < len(sorted_all_chapters):
+      chapter = sorted_all_chapters[i]
+      in_interval = chapter <= end_chapter
+      
+      if chapter > start_chapter and in_interval:
+        not_found_end_chapter = math.ceil(chapter - 1)
+        if not_found_end_chapter < start_chapter:
+          not_found_end_chapter = start_chapter
+        not_found_chapter_intervals.append((start_chapter, not_found_end_chapter))
+
+      next_int_chapter = None
+
+      # add chapters while they are included in the interval
+      while in_interval:
+        found_chapters.append(chapter)
+
+        # add chapters in between as not found
+        if next_int_chapter is not None and next_int_chapter < chapter:
+          not_found_chapter_intervals.append((next_int_chapter, math.ceil(chapter - 1)))
+        
+        # next chapter
+        i += 1
+        if i < len(sorted_all_chapters):
+          next_int_chapter = math.floor(chapter + 1)
+          chapter = sorted_all_chapters[i]
+          in_interval = chapter <= end_chapter
+        else:
+          in_interval = False
+      
+      # add the interval chapters that cannot be found
+      last_chapter_found = found_chapters[-1] if found_chapters else None
+      if not found_chapters or last_chapter_found < start_chapter:
+        not_found_chapter_intervals.append((start_chapter, end_chapter))
+      elif last_chapter_found < end_chapter:
+        not_found_start_chapter = math.floor(last_chapter_found + 1)
+        if not_found_start_chapter > end_chapter:
+          not_found_start_chapter = end_chapter
+        not_found_chapter_intervals.append((not_found_start_chapter, end_chapter))
+    else:
+      not_found_chapter_intervals.append((start_chapter, end_chapter))
+  
+  if not_found_chapter_intervals:
+    not_found_chapter_intervals = merge_intervals(not_found_chapter_intervals)
+
+  return found_chapters, not_found_chapter_intervals
 
 def split_rotate_2_pages(rotate):
   return str(1 if rotate else 0)
@@ -312,6 +410,7 @@ def online_search():
 if __name__ == "__main__":
 
   cancellable()
+  freeze_support()
   init_console_colors()
   
   # PARSE ARGS
@@ -320,12 +419,10 @@ if __name__ == "__main__":
 
   MANGA_DIR = strip_path(args.directory, DIRECTORY_KEEP)
 
-  CHAPTERS = set()
-
   if not args.profile:
     args.profile = 'KPW'
 
-  MANGA = args.manga
+  MANGA = ' '.join(args.manga)
 
   # SEARCH ANIME
 
@@ -352,7 +449,7 @@ if __name__ == "__main__":
       if manga_href is None:
         not_found()
       manga = manga_href.split('/')[-2] # encoded title
-      uuid = manga_href.split('/')[-1]
+      manga_uuid = manga_href.split('/')[-1]
       manga_title = result.find('h4').get_text().strip() # may contain special characters
       if manga_title.upper() == MANGA.upper():
         match = True
@@ -377,69 +474,65 @@ if __name__ == "__main__":
   directory = os.path.abspath(manga_directory(manga))
 
   if args.cache:
-    all_chapters = [int(chapter[0]) for chapter in folders(directory)]
+    ALL_CHAPTERS = [float(chapter[0]) for chapter in folders(directory)]
   else:
-    chapters_json = SCRAPER.get(CHAPTERS_WEBSITE + uuid)
+    chapters_json = SCRAPER.get(CHAPTERS_WEBSITE + manga_uuid)
     exit_if_fails(chapters_json)
     chapters_full = load_json(chapters_json.content, 'data', 'result')
-    all_chapters = [int(chapter['Number']) for chapter in chapters_full]
+    CHAPTERS_IDS = { float(chapter['Number']): chapter['Identification'] for chapter in chapters_full }
+    ALL_CHAPTERS = CHAPTERS_IDS.keys()
 
-  last = max(all_chapters)
-  if args.cache:
-    print_colored(f'Last downloaded chapter: {last}', Fore.YELLOW, Style.BRIGHT);
-
-  if args.chapters:
-    parse_chapters_range(args.chapters, last)
-  else:
-    CHAPTERS.update(all_chapters)
+  if not ALL_CHAPTERS:
+    error(f"There are no chapters of '{manga_title}' available {search_type}")
   
-  CHAPTERS = sorted(CHAPTERS)
+  ALL_CHAPTERS = sorted(ALL_CHAPTERS)
+
+  last = ALL_CHAPTERS[-1]
+  
+  CHAPTER_INTERVALS = parse_chapter_intervals(' '.join(args.chapters), last) if args.chapters else get_chapter_intervals(ALL_CHAPTERS)
+
+  CHAPTERS, chapters_not_found_intervals = chapters_in_intervals(ALL_CHAPTERS, CHAPTER_INTERVALS)
 
   if args.cache:
-    for chapter in CHAPTERS:
-      check_exists_dir(chapter_directory(manga, chapter))
+    print_colored(f'Last downloaded chapter: {last:g}', Fore.YELLOW, Style.BRIGHT)
   else:
-    # DOWNLOAD CHAPTERS
-
-    uuids = {}
-    for chapter in chapters_full:
-      number = chapter['Number']
-      if number in CHAPTERS:
-        uuids[number] = chapter['Identification']
-
     print_dim(f'{len(CHAPTERS)} chapter{plural(len(CHAPTERS))} will be downloaded - Cancel with Ctrl+C')
 
-    if not args.cache:
-      downloaded = False
+  if chapters_not_found_intervals:
+    chapters_not_found_intervals = join_chapter_intervals(chapters_not_found_intervals, interval_sep=', ')
+    not_found = 'are not downloaded' if args.cache else 'could not be found'
+    print_colored(f'The following chapters {not_found}: {chapters_not_found_intervals}', Fore.RED, Style.BRIGHT)
+    if args.cache:
+      error(f'Please download those chapters first.', 'Try again this command without --cache')
+    else:
+      print_colored('🖐️  Press enter to continue without those chapters or Ctrl+C to abort...', Fore.MAGENTA, Style.BRIGHT, end=' ')
+      input()
+  
+  if not CHAPTERS:
+    error("No chapters found")
 
-      for chapter in CHAPTERS:
-        uuid = uuids.get(chapter)
-        if uuid is None:
-          print_colored(f'{manga_title} {chapter} not found', Fore.RED)
-        else:
-          print_colored(f'Downloading {manga_title} {chapter}', Fore.YELLOW, Style.BRIGHT)
+  if not args.cache:
+    # DOWNLOAD CHAPTERS
 
-          url = CHAPTER_PAGES_WEBSITE + uuid
+    for chapter in CHAPTERS:
+      print_colored(f'Downloading {manga_title} {chapter:g}', Fore.YELLOW, Style.BRIGHT)
 
-          chapter_dir = chapter_directory(manga, chapter)
-          page = SCRAPER.get(url)
-          if success(page, print_ok=False):
-            html = BeautifulSoup(page.content, 'html.parser')
-            pages = html.find(id='PageList').find_all(True, recursive=False)
-            for page in pages:
-              page_id = page.get('value')
-              page_number = int(page.get_text())
-              url = IMAGE_WEBSITE + page_id
-              download(page_number, url, chapter_dir, text=f'Page {page_number}/{len(pages)} ({100*page_number//len(pages)}%)')
-            downloaded = True
+      url = CHAPTER_PAGES_WEBSITE + CHAPTERS_IDS[chapter]
 
-      if not downloaded:
-        error("No chapters found")
+      chapter_dir = chapter_directory(manga, chapter)
+      page = SCRAPER.get(url)
+
+      if success(page, print_ok=False):
+        html = BeautifulSoup(page.content, 'html.parser')
+        pages = html.find(id='PageList').find_all(True, recursive=False)
+        for page in pages:
+          page_id = page.get('value')
+          page_number = int(page.get_text())
+          url = IMAGE_WEBSITE + page_id
+          download(page_number, url, chapter_dir, text=f'Page {page_number}/{len(pages)} ({100*page_number//len(pages)}%)')
 
   extension = f'.{args.format.lower()}'
   args.format = args.format.upper()
-
-  chapter_range = filename_chapter_range(CHAPTERS)
 
   if args.format != 'PNG':
     print_colored(f'Converting to {args.format}...', Fore.BLUE, Style.BRIGHT)
@@ -451,21 +544,20 @@ if __name__ == "__main__":
       chapters_paths = []
       for chapter in CHAPTERS:
         chapter_dir = chapter_directory(manga, chapter)
-        page_number_paths = sorted(list(files(chapter_dir, 'png')), key=lambda name_path: int(name_path[0]))
-        page_paths = list(map(lambda name_path: name_path[1], page_number_paths))
+        page_number_paths = sorted(list(files(chapter_dir, 'png')), key=lambda page_path: int(page_path[0]))
+        page_paths = list(map(lambda page_path: page_path[1], page_number_paths))
         if args.single:
           chapters_paths.extend(page_paths)
         else:
-          path = f'{MANGA_DIR}/{manga_title} {chapter}{extension}'
+          path = f'{MANGA_DIR}/{manga_title} {chapter:g}{extension}'
           convert_to_pdf(path, page_paths)
       if args.single:
-        path = f'{MANGA_DIR}/{manga_title} {chapter_range}{extension}'
+        chapter_interval = chapters_to_intervals_string(CHAPTERS)
+        path = f'{MANGA_DIR}/{manga_title} {chapter_interval}{extension}'
         convert_to_pdf(path, chapters_paths)
     else:
       # CONVERT TO E-READER FORMAT
       from kindlecomicconverter.comic2ebook import main as manga2ebook
-
-      freeze_support()
 
       argv = ['--output', MANGA_DIR, '-p', args.profile, '--manga-style', '--hq', '-f', args.format, '--batchsplit', single(args.single), '-u', '-r', split_rotate_2_pages(args.rotate)]
       
@@ -473,25 +565,29 @@ if __name__ == "__main__":
         argv.append('-s')
 
       if args.single:
+        chapter_interval = chapters_to_intervals_string(CHAPTERS)
         with tempfile.TemporaryDirectory() as temp:
           copy_all([(chapter, chapter_directory(manga, chapter)) for chapter in CHAPTERS], temp)
-          title = f'{manga_title} {chapter_range}'
+          title = f'{manga_title} {chapter_interval}'
           print_colored(title, Fore.BLUE)
           argv = argv + ['--title', title, temp] # all chapters in manga directory are packed
           cache_convert(argv)
-          path = f'{MANGA_DIR}/{manga_title} {chapter_range}{extension}'
+          path = f'{MANGA_DIR}/{manga_title} {chapter_interval}{extension}'
           os.rename(f'{MANGA_DIR}/{os.path.basename(temp)}{extension}', path)
           print_colored(f'DONE: {os.path.abspath(path)}', Fore.GREEN, Style.BRIGHT)
       else:
         for chapter in CHAPTERS:
-          title = f'{manga_title} {chapter}'
+          title = f'{manga_title} {chapter:g}'
           print_colored(title, Fore.BLUE)
           argv_chapter = argv + ['--title', title, chapter_directory(manga, chapter)]
           cache_convert(argv_chapter)
-          path = f'{MANGA_DIR}/{manga_title} {chapter}{extension}'
-          os.rename(f'{MANGA_DIR}/{chapter}{extension}', path)
+          path = f'{MANGA_DIR}/{manga_title} {chapter:g}{extension}'
+          os.rename(f'{MANGA_DIR}/{chapter:g}{extension}', path)
           print_colored(f'DONE: {os.path.abspath(path)}', Fore.GREEN, Style.BRIGHT)
   else:
     if len(CHAPTERS) == 1:
       directory = os.path.abspath(chapter_directory(manga, CHAPTERS[0]))
-    print_colored(f'DONE: {directory}', Fore.GREEN, Style.BRIGHT)
+      chapter_intervals_info = ''
+    else:
+      chapter_intervals_info = f" ({chapters_to_intervals_string(CHAPTERS, interval_sep=', ')})"
+    print_colored(f'DONE: {directory}{chapter_intervals_info}', Fore.GREEN, Style.BRIGHT)
